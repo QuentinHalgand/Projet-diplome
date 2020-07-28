a7dd6a6fc5271fdef9fe9e60fb99d163e51a804e
Quentin Halgand <quentin.halgande@lapiscine.pro>
2020-07-28 17:22:15 +0200

    update du 28/07/20

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function Home()
+    {
+        return $this->render('home.html.twig');
+
+    }
+
+
+}
